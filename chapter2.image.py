1f4b26d65a057e04c81c91f67fee686393e86ebe
SYAHRULHASBIR <138756941+SYAHRULHASBIR@users.noreply.github.com>
2024-08-10 09:10:58 +0000

    task2 selesai

--- chapter2.image.py
+++ chapter2.image.py
@@ -3,5 +3,16 @@ from PIL import Image
 # Memuat gambar
 image = Image.open('/workspaces/multimedia-python/image.jpg')
 
+cropped_image = image.crop((10, 10, 200, 200))
+cropped_image.save('cropped_result.jpg')
+
+resized_image = cropped_image.resize((100, 100))
+resized_image.save('resized_result.jpg')
+
+from PIL import ImageFilter
+
+filtered_image = resized_image.filter(ImageFilter.BLUR)
+filtered_image.save('filtered_result.jpg')
+
 # Menyimpan gambar
 image.save('result.jpg')
